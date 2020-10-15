fe9cf63adbe77e1b350067bed9d3c03e78f329a3
liuzhao2580 <1457639270@qq.com>
2020-10-15 16:05:24 +0800

    天气调用报错的返回信息添加为中文

--- app/controller/gadget.js
+++ app/controller/gadget.js
@@ -1,13 +1,17 @@
 const Controller = require("egg").Controller
 const { data_success, data_failed } = require("../utils/reponse_data")
-const seniverseApi  = require("../utils/seniverse-api")
+const {seniverseApi, errorList}  = require("../utils/seniverse-api")
 class Gadget extends Controller {
     // 获取天气 使用的是心知天气提供的api https://www.seniverse.com/docs
     async get_Weather(){
         const {ctx} = this
         const {location} = ctx.query
-        const response = await seniverseApi(location).weather.now.data()
-        ctx.body = data_success(response)
+        try {
+            const response = await seniverseApi(location).weather.now.data()
+            ctx.body = data_success(response)
+        } catch (error) {
+            ctx.body = data_failed(100, errorList[error.signal])
+        }
     }
 }
 
--- app/utils/seniverse-api.js
+++ app/utils/seniverse-api.js
@@ -1,5 +1,5 @@
-const {SeniverseV3}  = require("seniverse-api")
-const seniverseApi = new SeniverseV3({
+const { SeniverseV3 } = require('seniverse-api')
+const setSeniverseApi = new SeniverseV3({
     encryption: {
         uid: 'PhO-St0kgR8oPvLPw', // 公钥
         key: 'SMcZkWoIk_3o4lMX2', // 私钥
@@ -11,7 +11,33 @@ const seniverseApi = new SeniverseV3({
     }
 })
 
-module.exports = (location)=> {
-    seniverseApi.options.query.location = location
-    return seniverseApi
+
+module.exports = {
+    seniverseApi(location){
+        setSeniverseApi.options.query.location = location
+        return setSeniverseApi
+    },
+    errorList: {
+        AP010001: 'API 请求参数错误',
+        AP010002: '没有权限访问这个 API 接口。在此查看你有权访问的 API 接口',
+        AP010003: 'API 密钥 key 错误',
+        AP010004: '签名错误',
+        AP010005: '你请求的 API 不存在',
+        AP010006: '没有权限访问这个地点',
+        AP010007: 'JSONP 请求需要使用签名验证方式',
+        AP010008: '没有绑定域名。请在控制台对应的产品管理下进行域名绑定',
+        AP010009: 'API 请求的 user-agent 与你设置的不一致',
+        AP010010: '没有这个地点',
+        AP010011: '无法查找到指定 IP 地址对应的城市',
+        AP010012: '你的服务已经过期。在此续费',
+        AP010013: '访问量余额不足。在此购买',
+        AP010014: '访问频率超过限制',
+        AP010015: '暂不支持该城市的车辆限行信息',
+        AP010016: '暂不支持该城市的潮汐数据',
+        AP010017: '请求的坐标超出支持的范围',
+        AP100001: '系统内部错误：数据缺失',
+        AP100002: '系统内部错误：数据错误',
+        AP100003: '系统内部错误：服务内部错误',
+        AP100004: '系统内部错误：网关错误'
+    }
 }
