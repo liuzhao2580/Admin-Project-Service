ad9cc9be82a7bd65581c53a5ec4de68a7daf0def
liuzhao2580 <1457639270@qq.com>
2020-11-10 09:33:46 +0800

    设置token的过期时间默认为一天

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -1,17 +1,21 @@
 const jwt = require('jsonwebtoken')
 module.exports = {
     // 生成token
-    setToken(data, expiresIn = '1h') {
-        return jwt.sign(data, this.app.config.jwt.secret, { expiresIn})
+    setToken(data, expiresIn = '1d') {
+        return jwt.sign(data, this.app.config.jwt.secret, { expiresIn })
     },
     // 解密 token
     verifyToken(data) {
         const getToken = data.split('Bearer ')[1]
-        return jwt.verify(getToken, this.app.config.jwt.secret, (err, decoded)=> {
-            return new Promise((resolve,reject) => {
-                if(err) reject(err)
-                else resolve(decoded)
-            })
-        })
+        return jwt.verify(
+            getToken,
+            this.app.config.jwt.secret,
+            (err, decoded) => {
+                return new Promise((resolve, reject) => {
+                    if (err) reject(err)
+                    else resolve(decoded)
+                })
+            }
+        )
     }
 }
