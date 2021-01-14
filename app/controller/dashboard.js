9c7bf0992eba408a06dc0c10d4427fa7e24c71d7
liuzhao2580 <1457639270@qq.com>
2021-01-14 08:58:03 +0800

    调用获取Echarts的请求

--- app/controller/dashboard.js
+++ app/controller/dashboard.js
@@ -1,4 +1,5 @@
 const { data_success } = require('../utils/reponse_data')
+const { getMathRandom } = require('../utils/tool')
 
 const Controller = require('egg').Controller
 
@@ -6,9 +7,49 @@ class Dashboard extends Controller {
     /** Echarts 的数据 */
     async get_EchartsData() {
         const { ctx } = this
-        console.log(123, '123')
-        ctx.body = 123
+        const echartsData = {
+            line: [
+                {
+                    name: 'Vue',
+                    type: 'line',
+                    data: getMathRandom(1000, 9999, 7)
+                },
+                {
+                    name: 'React',
+                    type: 'line',
+                    data: getMathRandom(1000, 9999, 7)
+                },
+                {
+                    name: 'Angular',
+                    type: 'line',
+                    data: getMathRandom(1000, 9999, 7)
+                }
+            ],
+            bar: [
+                {
+                    name: 'Vue',
+                    type: 'bar',
+                    data: getMathRandom(1000, 9999, 7)
+                },
+                {
+                    name: 'React',
+                    type: 'bar',
+                    data: getMathRandom(1000, 9999, 7)
+                },
+                {
+                    name: 'Angular',
+                    type: 'bar',
+                    data: getMathRandom(1000, 9999, 7)
+                }
+            ],
+            pie: [
+                { value: getMathRandom(1000, 9999)[0], name: 'Vue' },
+                { value: getMathRandom(1000, 9999)[0], name: 'React' },
+                { value: getMathRandom(1000, 9999)[0], name: 'Angular' }
+            ]
+        }
+        ctx.body = data_success(echartsData)
     }
-} 
+}
 
 module.exports = Dashboard
