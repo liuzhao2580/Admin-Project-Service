e489a903912cae4e08bd46b14b7f77cc36b856b6
liuzhao2580 <1457639270@qq.com>
2021-02-04 15:39:08 +0800

    删除心知天气api

--- app/controller/gadget.ts
+++ app/controller/gadget.ts
@@ -1,17 +1,5 @@
 import { Controller } from 'egg'
-const { data_success, data_failed } = require('../utils/reponse_data')
-const { seniverseApi, errorList } = require('../utils/seniverse-api')
 export default class Gadget extends Controller {
-    // 获取天气 使用的是心知天气提供的api https://www.seniverse.com/docs
-    async get_Weather() {
-        const { ctx } = this
-        const { location } = ctx.query
-        try {
-            const response = await seniverseApi(location).weather.now.data()
-            ctx.body = data_success(response)
-        } catch (error) {
-            ctx.body = data_failed(100, errorList[error.signal])
-        }
-    }
+    async get_Weather() {}
 }
 
--- app/service/user.ts
+++ app/service/user.ts
@@ -2,13 +2,13 @@ import { IUser } from '../typescript/database/user.interface'
 import { IUserLoginParams } from '../typescript/interface/user/user-config.interface'
 import { Service } from 'egg'
 export default class UserService extends Service {
-    // 用户登录
+    /** 用户登录 */
     async userLogin(params: IUserLoginParams): Promise<IUser[]> {
         const { app } = this
         let userLoginInfo: IUser[] = await app.mysql.select('user', { where: params })
         return userLoginInfo
     }
-    // 获取用户信息
+    /** 获取用户信息 */
     async userInfo(params) {
         const { app } = this
         const userWhere = {
@@ -17,7 +17,7 @@ export default class UserService extends Service {
         let userInfo: IUser[] = await app.mysql.select('user', userWhere)
         return userInfo
     }
-    // 更新用户信息
+    /** 更新用户信息 */
     async updateUserInfo(params) {
         const { app } = this
         const { userId, ...updateParams } = params
